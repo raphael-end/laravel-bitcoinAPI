8379140a6dcbd8c1dc9ebfe98e12d09f4ad6d02c
Raphael de moura Teixeira <raphaeldemourateixeira@MacBook-Pro-de-Raphael.local>
2021-02-05 10:12:49 -0300

    Interagindo com API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function index(){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api.coindesk.com/v1/bpi/currentprice.json",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $decoded = json_decode($response, true);
+            $data = $decoded['bpi']["USD"]["rate"];
+            return view('home',['data'=>$data]);
+        }
+    }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel bitcoin api</title>
-</head>
-<body>
-    
-</body>
-</html>
+<p>o preço do bitcoin é {{ $data ?? 'gostosa' }}</p>
